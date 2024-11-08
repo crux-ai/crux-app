f5a928545f4b0e61e07e759d1b72f258d73a5df2
Jack Cordery <jack.redolf@gmail.com>
2024-11-08 11:44:13 +0000

    fix(commitlint): allow cases in commitlint messages

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,17 @@ import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      2,
+      'never',
+      [
+        'upper-case',
+        'pascal-case',
+        'start-case',
+      ],
+    ],
+  },
 };
 
 export default Configuration;
